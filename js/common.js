0be77df893d64c0e06227ca40eb734a7ecdd098e
Aloxaf <bailong104@gmail.com>
2018-08-29 10:07:50 +0800

    加入slides

--- js/common.js
+++ js/common.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
     }, function() {
         $(this).find('.dropdown-menu').stop(true, true).slideUp();
     });
+
+    $('.flexslider').flexslider({
+        animation: 'slide',
+        animationLoop: true,
+        itemMargin: 0,
+        slideshowSpeed: 4000,
+        animationSpeed: 500
+    })
 });
