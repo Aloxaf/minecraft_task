cac9836936951ff2ffda168025df4b91dd092a3f
Aloxaf <bailong104@gmail.com>
2018-08-29 14:42:17 +0800

    加入图标, 增加注释

--- js/common.js
+++ js/common.js
@@ -1,16 +1,15 @@
 
 $(document).ready(function(){
+    // 下拉菜单自动展开
     $('ul.nav li.dropdown').hover(function() {
         $(this).find('.dropdown-menu').stop(true, true).slideDown();
     }, function() {
         $(this).find('.dropdown-menu').stop(true, true).slideUp();
     });
 
+    // 自动播放幻灯片
     $('.flexslider').flexslider({
-        animation: 'slide',
-        animationLoop: true,
-        itemMargin: 0,
-        slideshowSpeed: 4000,
-        animationSpeed: 500
+        animation: 'slide',   // 自动滑动
+        slideshowSpeed: 3000  // 自动滑动间隔
     })
 });
